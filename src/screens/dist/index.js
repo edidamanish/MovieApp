9c0d723f31c654c5f5a53bb975d7fc659046f56a
Manish Edida <edidamanish@gmail.com>
2022-07-12 02:37:22 +0530

    removed dist files

--- src/screens/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.SearchScreen = exports.SettingsScreen = exports.HomeScreen = void 0;
-var home_screen_1 = require("./home-screen");
-exports.HomeScreen = home_screen_1["default"];
-var settings_screen_1 = require("./settings-screen");
-exports.SettingsScreen = settings_screen_1["default"];
-var search_screen_1 = require("./search-screen");
-exports.SearchScreen = search_screen_1["default"];
